f56476e0bcd0c1f186abb9d58b9b854cde5b8456
HongQuan78 <vhquan313@gmail.com>
2024-03-23 16:13:22 +0700

    Add project files.

---
+++ Length of Last Word/Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    static void Main()
+    {
+        string s = "a";
+        Console.WriteLine(LengthOfLastWord(s));
+    }
+
+    static int LengthOfLastWord(string s)
+    {
+        int lastSpaceIndex = 0;
+        s = s.Trim();
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == ' ')
+            {
+                lastSpaceIndex = i;
+            }
+        }
+        if(lastSpaceIndex == 0)
+        {
+            return s.Length;
+        }
+        return  s.Length - lastSpaceIndex - 1;
+    }
+}
